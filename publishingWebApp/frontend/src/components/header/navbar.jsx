ead0ff044f973b2500bdc83700c7965af7b48294
Hajira-Mirza <hajiramrz@gmail.com>
2024-03-24 20:41:41 +0500

    commit changes

--- publishingWebApp/frontend/src/components/header/navbar.jsx
+++ publishingWebApp/frontend/src/components/header/navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <>
-    <div className="w-screen h-14 bg-white flex items-center space-x-20 drop-shadow-xl">
+    <div className="w-screen h-14 bg-white flex justify-center items-center space-x-32 drop-shadow-xl">
       <div className="flex justify-center">
         <NavLink className="hover:border-2 border-violet-500 px-5 py-1 rounded-full text-violet-500 hover:text-violet-500 transition-all 
       focus:border-2 focus:border-violet-500" to={""}>Home</NavLink>
@@ -21,11 +21,17 @@ function Navbar() {
       </div>
       <div>
       <button
-      className="text-white border-2 bg-violet-500 border-violet-500 mt-2 rounded-full
+      className="text-white border-2 bg-violet-500 border-violet-500 flex-items-end justify-items-end rounded-full
       px-12 py-2 inline-block font-semi-bold hover:bg-white hover:text-violet-500 hover:drop-shadow-xl hover:border-violet-500 focus:outline-none"
         >Sign In</button>
       </div>
+      
+      <button
+      className="text-white border-2 bg-violet-500 border-violet-500 flex-items-end justify-items-end rounded-full
+      px-12 py-2 inline-block font-semi-bold hover:bg-white hover:text-violet-500 hover:drop-shadow-xl hover:border-violet-500 focus:outline-none"
+        >Sign Up</button>
     </div>
+
     </>
   );
 }
